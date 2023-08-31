72182ed60be69304c046ab13a3103ccac0b15049
claredavies <clare.davies@abo.fi>
2023-08-31 14:13:42 +0100

    Added delete button to side but issue where not assigned to individual chart

--- src/App.js
+++ src/App.js
@@ -57,6 +57,10 @@ function App() {
     setCharts(prevCharts => [...prevCharts, newChart]);
   };
 
+  const handleDeleteChart = (index) => {
+      setCharts(prevCharts => prevCharts.filter((_, idx) => idx !== index));
+    };
+
   return (
     <div className="App">
         <h1 className="app-title">AWS Services Price Dashboard</h1>
@@ -93,6 +97,7 @@ function App() {
         <div className="charts-wrapper">
             {charts.map((config, index) => (
               <div key={index} className="chart-container">
+                <button onClick={() => handleDeleteChart(index)} style={{position: 'absolute', right: '10px'}}>X</button>
                 {config.chartType === 'Bar' && <BarChart chartData={config.data} xAxisMetric={config.xAxisMetric} yAxisMetric={config.yAxisMetric}/>}
                 {config.chartType === 'Line' && <LineChart chartData={config.data} xAxisMetric={config.xAxisMetric} yAxisMetric={config.yAxisMetric}/>}
                 {config.chartType === 'Pie' && <PieChart chartData={config.data} xAxisMetric={config.xAxisMetric} yAxisMetric={config.yAxisMetric}/>}
