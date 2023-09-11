61c02be46ec3ba3cc7f56f94bcf17aad48994741
claredavies <clare.davies@abo.fi>
2023-09-11 12:45:02 +0100

    Added unit tests for useAppData to check that when initially render and change the datatype the data is transformed by transformData

--- src/customHooks/__tests__/useAppData.test.js
+++ src/customHooks/__tests__/useAppData.test.js
@@ -4,75 +4,65 @@ import { useAppData } from '../useAppData';
 import { mockData } from '../../mocks/mockJsonData'
 import transformData from '../../components/dataTransform';
 
-//TESTING
-//1. The hook fetches data correctly.
-//2. The hook calls the transformData function when necessary.
-//3. The transformed data is stored correctly.
-//4. You can mock the transformData function here just to ensure it's called, without bothering about the exact transformation.
-
 // Enable fetch mock
 fetchMock.enableMocks();
 
-// Reset fetch mock before each test
-beforeEach(() => {
-  fetchMock.resetMocks();
-});
-
-// jest needs to mock the module before mocking the function
 jest.mock('../../components/dataTransform', () => ({
-  __esModule: true, // this property makes it work smoothly with ES6 imports
+  __esModule: true,
   default: jest.fn()
 }));
 
-//Check if fetch is called on initial render
-// it is a function from Jest which defines a single test
-// second argument is a callback function
-it('fetches data on initial render', async () => {
-    // the next time fetch is called (in fetchData where call API using fetch) instead of returning the API response
-    // the mock response is returned
-    fetchMock.mockResponseOnce(JSON.stringify(mockData));
-
-    // render hook is used to test a custom Hook - in this case useAppData()
-    // renderHook returns an object containing several properties and utility methods to help with testing.
-    // waitForNextUpdate: This is one of the utility methods provided by the renderHook result.
-    // It's particularly useful for waiting for asynchronous operations to complete in the hook (like data fetching).
-    const { result, waitForNextUpdate } = renderHook(() => useAppData());
-
-    // Check that jsonData is initially null
-    expect(result.current.jsonData).toBeNull();
-
-    await waitForNextUpdate();
-
-    // Check that jsonData is now populated with mockData
-    expect(result.current.jsonData).toEqual(mockData);
-
-    // check fetch only called once (no unnecessary calls)
-    expect(fetchMock.mock.calls.length).toEqual(1);
-});
-
-it('calls the transformData function when necessary', async () => {
-    fetchMock.mockResponseOnce(JSON.stringify(mockData));
-
-    // Mock the implementation for this test case
-    transformData.mockImplementation((data, type) => {
-        return data;
+describe('useAppData hook', () => {
+    // Reset fetch mock and clear all mocks before each test
+    beforeEach(() => {
+      fetchMock.resetMocks();
+      jest.clearAllMocks();
     });
 
-    const { result, waitForNextUpdate } = renderHook(() => useAppData());
+    it('fetches data on initial render', async () => {
+        fetchMock.mockResponseOnce(JSON.stringify(mockData));
 
-    await waitForNextUpdate();
+        const { waitForNextUpdate } = renderHook(() => useAppData());
+        await waitForNextUpdate();
 
-    // Check if transformData was called
-    expect(transformData).toHaveBeenCalled();
+        expect(fetchMock.mock.calls.length).toEqual(1);
+    });
 
-    // Clean up mock after test
-    transformData.mockClear();
+    it('transforms data on initial render when json is not null', async () => {
+        fetchMock.mockResponseOnce(JSON.stringify(mockData));
+        transformData.mockImplementation((data, type) => data);
+
+        const { result, waitForNextUpdate } = renderHook(() => useAppData());
+
+        expect(result.current.jsonData).toBeNull();
+        expect(transformData).not.toHaveBeenCalled();
+
+        await waitForNextUpdate();
+
+        expect(result.current.jsonData).toEqual(mockData);
+        expect(transformData).toHaveBeenCalled();
+    });
+
+    describe('transformData calls based on selectedDataType changes', () => {
+        beforeEach(() => {
+            fetchMock.mockResponseOnce(JSON.stringify(mockData));
+            transformData.mockImplementation((data, type) => data);
+        });
+
+        it('calls transformData when selectedDataType changes', async () => {
+            const { result, waitForNextUpdate } = renderHook(() => useAppData());
+            await waitForNextUpdate();
+
+            expect(transformData).toHaveBeenCalledTimes(1);
+
+            act(() => result.current.setSelectedDataType('Applications'));
+            expect(transformData).toHaveBeenCalledTimes(2);
+
+            act(() => result.current.setSelectedDataType('Applications'));
+            expect(transformData).toHaveBeenCalledTimes(2);
+
+            act(() => result.current.setSelectedDataType('Instances'));
+            expect(transformData).toHaveBeenCalledTimes(3);
+        });
+    });
 });
-
-//check calls transformData when dataType changes
-
-
-
-
-
-
